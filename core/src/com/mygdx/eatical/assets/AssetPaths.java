78b8f7679e24593eb936f20d959c6175b429ef1b
kristijan-mitrov <kristijan.mitrov@infinum.com>
2022-12-27 23:44:31 +0100

    Implement UIand logic for Restaurant add & find

---
+++ core/src/com/mygdx/eatical/assets/AssetPaths.java
@@ -0,0 +1,12 @@
+package com.mygdx.eatical.assets;
+
+public class AssetPaths {
+
+    public static final String UI_SKIN = "ui/skin/cloud-form-ui.json";
+
+    public static final String UI_FONT = "ui/skin/font-export.fnt";
+
+
+    private AssetPaths() {
+    }
+}
--- core/src/utils/Geolocation.java
+++ core/src/utils/Geolocation.java
@@ -3,9 +3,18 @@ package utils;
 public class Geolocation {
     public double lat;
     public double lng;
+    public String name;
+    public String address;
 
     public Geolocation(double lat, double lng) {
         this.lat = lat;
         this.lng = lng;
+    }
+
+    public Geolocation(double lat, double lng, String name, String address) {
+        this.lat = lat;
+        this.lng = lng;
+        this.name = name;
+        this.address = address;
     }
 }
