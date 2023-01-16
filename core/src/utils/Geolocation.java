e18783a4fb849ccccc5e419f037516b0377e606e
kristijan-mitrov <kristijan.mitrov@infinum.com>
2023-01-16 08:42:42 +0100

    Add people traffic

--- core/src/utils/Geolocation.java
+++ core/src/utils/Geolocation.java
@@ -5,16 +5,18 @@ public class Geolocation {
     public double lng;
     public String name;
     public String address;
+    public int numOfPeople;
 
     public Geolocation(double lat, double lng) {
         this.lat = lat;
         this.lng = lng;
     }
 
-    public Geolocation(double lat, double lng, String name, String address) {
+    public Geolocation(double lat, double lng, String name, String address, int numOfPeople) {
         this.lat = lat;
         this.lng = lng;
         this.name = name;
+        this.numOfPeople = numOfPeople;
         this.address = address;
     }
 }
